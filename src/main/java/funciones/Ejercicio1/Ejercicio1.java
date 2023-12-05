bbb36ff92c639befc279c1e714f2990530943111
Cristobal Rosa <crisrogar@gmail.com>
2023-12-05 12:42:21 +0100

    update ejercicio1 funciones

--- src/main/java/funciones/Ejercicio1/Ejercicio1.java
+++ src/main/java/funciones/Ejercicio1/Ejercicio1.java
@@ -7,12 +7,14 @@ public class Ejercicio1 {
 
             - voltea: le da la vuelta a un numero
             - esCapicua: Devuelve verdadero si el numero que se pasa como parametro es capicuay falso en caso contrario
+            - esPrimo: Devuelve verdadero si el numero que se pasa como parametro es primo y false en caso contrario
      */
 
 
     public static void main(String[] args) {
         System.out.println(Matematicas.voltea(2332));
         System.out.println(Matematicas.esCapicua(2332));
+        System.out.println(Matematicas.esPrimo(1847));
     }
 
 }
--- src/main/java/funciones/Ejercicio1/Matematicas.java
+++ src/main/java/funciones/Ejercicio1/Matematicas.java
@@ -18,4 +18,12 @@ public class Matematicas {
             return true;
         return false;
     }
+
+    public static boolean esPrimo(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
 }
