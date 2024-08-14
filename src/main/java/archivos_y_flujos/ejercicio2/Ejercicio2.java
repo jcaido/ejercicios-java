dad864b10e80852d88277c9125dc942a000670e4
Cristobal Rosa <crisrogar@gmail.com>
2024-08-14 11:50:38 +0200

    ejercicio 2 archivos y flujos

---
+++ src/main/java/archivos_y_flujos/ejercicio2/Ejercicio2.java
@@ -0,0 +1,18 @@
+package archivos_y_flujos.ejercicio2;
+
+public class Ejercicio2 {
+
+    /*
+        Crear un archivo de acceso secuencial que muestre las cuentas a cobrar de varios clientes. Por cada cliente,
+        el programa obtendrá un número de cuenta, el nombre del cliente y su saldo. Además, por cada cliente se
+        añadirá un registro en un archivo.
+     */
+
+    public static void main(String[] args) {
+
+        CrearArchivoTexto aplicacion = new CrearArchivoTexto();
+        aplicacion.abrirArchivo();
+        aplicacion.agregarRegistros();
+        aplicacion.cerrarArchivo();
+    }
+}
