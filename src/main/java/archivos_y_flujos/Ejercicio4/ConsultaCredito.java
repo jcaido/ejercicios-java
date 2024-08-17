43048dfaefd9ede492b8c7605a4f624ca451106d
Cristobal Rosa <crisrogar@gmail.com>
2024-08-17 13:02:08 +0200

    update Ejercicio4 archivos y flujos

--- src/main/java/archivos_y_flujos/Ejercicio4/ConsultaCredito.java
+++ src/main/java/archivos_y_flujos/Ejercicio4/ConsultaCredito.java
@@ -2,6 +2,8 @@ package archivos_y_flujos.Ejercicio4;
 
 import com.sun.source.tree.WhileLoopTree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -36,6 +38,52 @@ public class ConsultaCredito {
 
     private void leerRegistros() {
 
+        RegistroCuenta registroCuenta = new RegistroCuenta();
+
+        try {
+            Scanner entrada = new Scanner(new File("clientes.txt"));
+            while (entrada.hasNext()) {
+                registroCuenta.setCuenta(entrada.nextInt());
+                registroCuenta.setPrimerNombre(entrada.next());
+                registroCuenta.setApellidoPaterno(entrada.next());
+                registroCuenta.setSaldo(entrada.nextDouble());
+
+                if (debeMostrarRegistro(registroCuenta.getSaldo())) {
+                    System.out.printf("%-10d%-12s%-12s%10.2f\n",
+                            registroCuenta.getCuenta(),
+                            registroCuenta.getPrimerNombre(),
+                            registroCuenta.getApellidoPaterno(),
+                            registroCuenta.getSaldo());
+                }
+            }
+        } catch (NoSuchElementException noSuchElementException) {
+            System.err.println("El archivo no esta bien formado");
+            entrada.close();
+            System.exit(1);
+        } catch (IllegalStateException illegalStateException) {
+            System.err.println("Error al leer el archivo");
+            System.exit(1);
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.err.println("No se puede encontrar el archivo");
+            System.exit(1);
+        } finally {
+            if (entrada != null)
+                entrada.close();
+        }
+    }
+
+    private boolean debeMostrarRegistro(double saldo) {
+        if (saldo == 0 && this.tipoCuenta == OpcionMenu.SALDO_CERO)
+            return true;
+
+        else if (saldo < 0 && this.tipoCuenta == OpcionMenu.SALDO_CREDITO)
+            return true;
+
+        else if (saldo > 0 && this.tipoCuenta == OpcionMenu.SALDO_DEBITO)
+            return true;
+
+        else
+            return false;
     }
 
     public void procesarSolicitudes() {
