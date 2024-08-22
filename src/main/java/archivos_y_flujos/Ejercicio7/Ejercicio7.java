bfe372791883c6e94e1829da58602ebb3552029e
Cristobal Rosa <crisrogar@gmail.com>
2024-08-22 20:03:49 +0200

    Ejercicio7 archivos y flujos

---
+++ src/main/java/archivos_y_flujos/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,16 @@
+package archivos_y_flujos.Ejercicio7;
+
+public class Ejercicio7 {
+
+    /*
+        Escribir datos a un fichero de texto. Si el archivo ya existe, utilizar el objeto escribirFicheroExistente
+     */
+
+    public static void main(String[] args) {
+
+        EscribirFichero escribirFichero = new EscribirFichero();
+        escribirFichero.escribe();
+        //EscribirFicheroExistente escribirFicheroExistente = new EscribirFicheroExistente();
+        //escribirFicheroExistente.escribir();
+    }
+}
---
+++ src/main/java/archivos_y_flujos/Ejercicio7/EscribirFichero.java
@@ -0,0 +1,21 @@
+package archivos_y_flujos.Ejercicio7;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribirFichero {
+
+    public void escribe() {
+        try {
+            String frase = "Esto es una prueba de escritura. ";
+            FileWriter salida = new FileWriter("C:/Users/Usuario/Desktop/ejemplo.txt");
+
+            for (int i = 0; i < frase.length(); i++) {
+                salida.write(frase.charAt(i));
+            }
+            salida.close();
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo");
+        }
+    }
+}
