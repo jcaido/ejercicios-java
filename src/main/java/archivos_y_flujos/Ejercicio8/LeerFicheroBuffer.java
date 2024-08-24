f2a80bdfe128062e72da9fff2b42426d1fd171df
Cristobal Rosa <crisrogar@gmail.com>
2024-08-24 18:23:00 +0200

    Ejercicio8 archivos y flujos

---
+++ src/main/java/archivos_y_flujos/Ejercicio8/LeerFicheroBuffer.java
@@ -0,0 +1,33 @@
+package archivos_y_flujos.Ejercicio8;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFicheroBuffer {
+
+    public void leer() {
+
+        try {
+            FileReader entrada = new FileReader("C:/Users/Usuario/Desktop/ejemplo.txt");
+            BufferedReader miBuffer = new BufferedReader(entrada);
+
+            String linea = "";
+            while (linea != null) {
+                linea = miBuffer.readLine();
+
+                if (linea != null)
+                    System.out.println(linea);
+            }
+
+            entrada.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al abrir el archivo");
+        }
+
+    }
+
+
+}
