5be65dd60f68516a56067cc3ad1219be2967d68f
Cristobal Rosa <crisrogar@gmail.com>
2024-09-18 11:39:41 +0200

    Ejericio05 Colecciones

--- src/main/java/Colecciones/Ejercicio05/Ejercicio05.java
+++ src/main/java/Colecciones/Ejercicio05/Ejercicio05.java
@@ -1,2 +1,37 @@
-package Colecciones.Ejercicio05;public class Ejercicio05 {
+package Colecciones.Ejercicio05;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Ejercicio05 {
+
+    /*
+
+     */
+
+    public static void main(String[] args) {
+
+        LinkedList<String> paises = new LinkedList<>();
+        paises.add("España");
+        paises.add("Colombia");
+        paises.add("Mexico");
+        paises.add("Peru");
+        ListIterator<String> iterA = paises.listIterator();
+
+        LinkedList<String> capitales = new LinkedList<>();
+        capitales.add("Madrid");
+        capitales.add("Bogota");
+        capitales.add("DF");
+        capitales.add("Lima");
+        ListIterator<String> iterB = capitales.listIterator();
+
+        while (iterB.hasNext()) {
+            if (iterA.hasNext()) {
+                iterA.next();
+            }
+            iterA.add(iterB.next());
+        }
+
+        System.out.println(paises);
+    }
 }
