71b2432ff00a1d113cdb7b414e39caa5b7f973d4
Cristobal Rosa <crisrogar@gmail.com>
2024-10-07 20:52:00 +0200

    Ejercicio03 colecciones y diccionarios

--- src/main/java/coleccionesydiccionarios/Ejercicio03.java
+++ src/main/java/coleccionesydiccionarios/Ejercicio03.java
@@ -1,2 +1,30 @@
-package coleccionesydiccionarios;public class Ejercicio03 {
+package coleccionesydiccionarios;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ejercicio03 {
+
+    /*
+        Escribe un programa que ordene 10 números enteros introducidos por teclado y almacenados en un objeto de la
+        case ArrayList.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> miArrayList = new ArrayList<>();
+
+        for (int i=0; i<10; i++) {
+            System.out.println("Introduza un numero entero");
+            int numero = scanner.nextInt();
+            miArrayList.add(numero);
+        }
+
+        Collections.sort(miArrayList);
+
+        for (int elemento: miArrayList)
+            System.out.println(elemento);
+    }
 }
