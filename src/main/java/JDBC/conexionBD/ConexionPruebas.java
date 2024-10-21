04dea371449be5994ebaa020dec914bd1fedf6ba
Cristobal Rosa <crisrogar@gmail.com>
2024-10-21 10:54:30 +0200

    update conexionBD, JDBC

--- src/main/java/JDBC/conexionBD/ConexionPruebas.java
+++ src/main/java/JDBC/conexionBD/ConexionPruebas.java
@@ -7,12 +7,14 @@ public class ConexionPruebas {
     public static void main(String[] args) {
 
         try {
-            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "admin" );
+            //Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "admin" );
+            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost/pruebas", "root", "admin" );
             Statement miStatement = miConexion.createStatement();
             ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM productos");
 
             while (miResultSet.next()) {
-                System.out.println(miResultSet.getString("CODIGOARTICULO") + " " + miResultSet.getString("NOMBREARTICULO"));
+                //System.out.println(miResultSet.getString("CODIGOARTICULO") + " " + miResultSet.getString("NOMBREARTICULO"));
+                System.out.println(miResultSet.getString(2) + " " + miResultSet.getString(3));
             }
 
         } catch (SQLException e) {
